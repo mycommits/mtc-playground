d15de7a81b4bb0eb66447fdd05780727183c1719
Charlie Chahardoli <charliechahardoli@gmail.com>
2024-11-25 02:03:08 +0000

    Add random_id, index_html resources

--- templates/main.tf
+++ templates/main.tf
@@ -1,6 +1,26 @@
+resource "random_id" "random" {
+  byte_length = 2
+}
+
 resource "github_repository" "mtc_repo" {
-  name        = "mtc-repo"
+  name        = "mtc_repo_${random_id.random.dec}"
   description = "Code for MTC"
   visibility  = "private"
   auto_init   = true
+}
+
+resource "github_repository_file" "read_me" {
+  repository          = github_repository.mtc_repo.name
+  branch              = "master"
+  file                = "README.md"
+  content             = "# Infrastructure Developer Repository"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "index_html" {
+  repository          = github_repository.mtc_repo.name
+  branch              = "master"
+  file                = "index.html"
+  content             = "Hello Terraform!"
+  overwrite_on_create = true
 }
